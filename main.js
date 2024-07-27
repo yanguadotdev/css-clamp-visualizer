d01bfd161e10afbe52e56dca5828be9c93a8b83d
Creatlydev <creatlydev@gmail.com>
2024-07-27 16:05:12 -0500

    rafactor: javascript file

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const $textElement = document.querySelector('.font-viewer__text')
+const $max = document.querySelector('.max')
+const $min = document.querySelector('.min')
+const $ideal = document.querySelector('.ideal')
+const $minInput = document.querySelector('.min input')
+const $maxInput = document.querySelector('.max input')
+
 const updateValues = () => {
   let pageWidth = window.innerWidth
-  let textElement = document.querySelector('.font-viewer__text')
-  let fontSizeText = getComputedStyle(textElement).fontSize.slice(0, -2)
+  let fontSizeText = getComputedStyle($textElement).fontSize.slice(0, -2)
 
   document.querySelector('.window').innerText = pageWidth + 'px'
   document.querySelector('.actual').innerText = `${Math.round(
@@ -12,20 +18,19 @@ const updateValues = () => {
 }
 
 const checkFontSize = (fontSizeText) => {
-  const minVal = parseInt(document.querySelector('.min input').value)
-  const maxVal = parseInt(document.querySelector('.max input').value)
+  const minVal = parseInt($minInput.value)
+  const maxVal = parseInt($maxInput.value)
 
   const removeClass = (except) => {
-    document.querySelector('.max').classList.remove('highlight')
-    document.querySelector('.min').classList.remove('highlight')
-    document.querySelector('.ideal').classList.remove('highlight')
+    $max.classList.remove('highlight')
+    $min.classList.remove('highlight')
+    $ideal.classList.remove('highlight')
     if (except) except.classList.add('highlight')
   }
 
-  if (fontSizeText === maxVal) removeClass(document.querySelector('.max'))
-  else if (fontSizeText === minVal) removeClass(document.querySelector('.min'))
-  else if (maxVal > fontSizeText > minVal) removeClass(document.querySelector('.ideal'))
-  else removeClass(document.querySelector('.ideal'))
+  if (fontSizeText === maxVal) removeClass($max)
+  else if (fontSizeText === minVal) removeClass($min)
+  else removeClass($ideal)
 }
 
 updateValues()
